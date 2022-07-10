9a87b4cb6cd71fdb5440558e50eb6fa11ef47eff
jamesslinger <jamesxslinger@gmail.com>
2022-07-10 19:24:16 +0100

    Updating git

--- server.py
+++ server.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import datetime as dt
+import git
 
 app = Flask(__name__)
+
+
+@app.route("/personal_site", methods=["POST"])
+def webhook():
+    if request.method == "POST":
+        repo = git.Repo("https://github.com/jamesslinger/Personal-Site-v2.0.git")
+        origin = repo.remotes.origin
+        origin.pull()
+        return "Updated PA successfully", 200
+    else:
+        return "Wrong event type", 400
 
 
 @app.route("/")
